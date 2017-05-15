1d4762f08decfee157c8c19dccc9efa058ad386c
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-05-15 18:35:59 -0500

    feat(Events): add default events and issue events to avoid coupling layer

---
+++ src/main/java/org/example/seed/event/RequestEvent.java
@@ -0,0 +1,15 @@
+package org.example.seed.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+/**
+ * Created by Ricardo Pina Arellano on 30/11/2016.
+ */
+@Data
+@AllArgsConstructor
+public abstract class RequestEvent {
+    private String id;
+}
---
+++ src/main/java/org/example/seed/event/issue/DeleteIssueEvent.java
@@ -0,0 +1,17 @@
+package org.example.seed.event.issue;
+
+import lombok.Builder;
+import org.example.seed.event.DeleteEvent;
+
+import java.util.UUID;
+
+/**
+ * Created by Ricardo Pina Arellano on 30/11/2016.
+ */
+public class DeleteIssueEvent extends DeleteEvent {
+
+    @Builder
+    public DeleteIssueEvent(final String id) {
+        super(id);
+    }
+}
