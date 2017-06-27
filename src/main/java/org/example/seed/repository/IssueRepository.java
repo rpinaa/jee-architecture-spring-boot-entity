df2b4b286e79b483b4e6a4232bcd60ab9a7c9ee3
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-26 21:16:41 -0500

    fix(domain): remove all Issue domain

--- src/main/java/org/example/seed/repository/IssueRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.seed.repository;
-
-import org.example.seed.domain.Issue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IssueRepository extends JpaRepository<Issue, String> {
-}
