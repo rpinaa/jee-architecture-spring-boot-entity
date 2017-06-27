728235b7992a2615a9f706b165be9187ddcaf244
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-27 13:49:01 -0500

    fix(domain): add validation groups

--- src/main/java/org/example/seed/domain/Telephone.java
+++ src/main/java/org/example/seed/domain/Telephone.java
@@ -20,19 +20,19 @@ public class Telephone {
         this.id = UUID.randomUUID().toString();
     }
 
-    @Size(min = 36, max = 36)
+    @Size(min = 36, max = 36, groups = {ClientUpdateGroup.class, ChefUpdateGroup.class})
     @NotNull(groups = {ClientUpdateGroup.class, ChefUpdateGroup.class})
     private String id;
 
-    @Size(min = 2, max = 15)
+    @Size(min = 2, max = 15, groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
     @NotNull(groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
     private String name;
 
-    @Size(min = 5, max = 12)
+    @Size(min = 5, max = 12, groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
     @NotNull(groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
     private String number;
 
-    @Size(min = 2, max = 10)
+    @Size(min = 2, max = 10, groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
     @NotNull(groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
     private String lada;
 
