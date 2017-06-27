45379ce9aaae92cfaf0308a6480014138b95a3c6
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-26 21:21:09 -0500

    feat(repository): create Telephone repository from JPA

---
+++ src/main/java/org/example/seed/repository/TelephoneRepository.java
@@ -0,0 +1,12 @@
+package org.example.seed.repository;
+
+import org.example.seed.entity.TelephoneEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by PINA on 25/06/2017.
+ */
+@Repository
+public interface TelephoneRepository extends JpaRepository<TelephoneEntity, String> {
+}
