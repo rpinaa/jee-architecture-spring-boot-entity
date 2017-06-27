93f1c62361a54941e2078e6c80edd5e841916128
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-26 21:17:18 -0500

    feat(domain): add new domain layer without entity

---
+++ src/main/java/org/example/seed/domain/Telephone.java
@@ -0,0 +1,41 @@
+package org.example.seed.domain;
+
+import lombok.Data;
+import org.example.seed.catalog.TelephoneType;
+import org.example.seed.group.chef.ChefUpdateGroup;
+import org.example.seed.group.client.ClientCreateGroup;
+import org.example.seed.group.client.ClientUpdateGroup;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+/**
+ * Created by PINA on 26/06/2017.
+ */
+@Data
+public class Telephone {
+
+    public Telephone() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    @Size(min = 36, max = 36)
+    @NotNull(groups = {ClientUpdateGroup.class, ChefUpdateGroup.class})
+    private String id;
+
+    @Size(min = 2, max = 15)
+    @NotNull(groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
+    private String name;
+
+    @Size(min = 5, max = 12)
+    @NotNull(groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
+    private String number;
+
+    @Size(min = 2, max = 10)
+    @NotNull(groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
+    private String lada;
+
+    @NotNull(groups = {ClientCreateGroup.class, ChefUpdateGroup.class})
+    private TelephoneType type;
+}
