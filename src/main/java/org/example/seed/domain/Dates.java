037e781c319b97d1e6d2211319fcd4e3f1880cfc
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-26 21:24:26 -0500

    feat(domain): add Dates super class to bring create and change Date

---
+++ src/main/java/org/example/seed/domain/Dates.java
@@ -0,0 +1,14 @@
+package org.example.seed.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Created by PINA on 26/06/2017.
+ */
+@Data
+public class Dates {
+    private Date createDate;
+    private Date changeDate;
+}
