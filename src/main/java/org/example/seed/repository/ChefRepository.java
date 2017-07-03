204aad0000c33f68a6da0675e253a470ff9078bc
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-07-02 20:56:20 -0500

    feat(repository): create custom method to find chef by dish id

--- src/main/java/org/example/seed/repository/ChefRepository.java
+++ src/main/java/org/example/seed/repository/ChefRepository.java
@@ -2,6 +2,7 @@ package org.example.seed.repository;
 
 import org.example.seed.entity.ChefEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ChefRepository extends JpaRepository<ChefEntity, String> {
+
+    @Query("SELECT ce FROM ChefEntity ce LEFT JOIN ce.account AS ae JOIN ce.dishes AS se WHERE ce.active = TRUE AND se.id = ?1")
+    ChefEntity findOneByDish(final String idDish);
 }
