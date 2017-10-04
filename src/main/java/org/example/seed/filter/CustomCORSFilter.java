3bbb55f850fd12a9a30545351c878c8dc47d3887
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-10-04 00:11:10 -0500

    style(filter): apply 2 lines indentation

--- src/main/java/org/example/seed/filter/CustomCORSFilter.java
+++ src/main/java/org/example/seed/filter/CustomCORSFilter.java
@@ -9,22 +9,22 @@ import java.io.IOException;
 @Component
 public class CustomCORSFilter implements Filter {
 
-	@Override
-	public void destroy() { }
+  @Override
+  public void destroy() { }
 
-	@Override
-	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+  @Override
+  public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
 
-		final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+    final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
-		httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-		httpServletResponse.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
-		httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-		httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+    httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+    httpServletResponse.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
+    httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+    httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
 
-		chain.doFilter(request, response);
-	}
+    chain.doFilter(request, response);
+  }
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException { }
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException { }
 }	
