292634c939e6f5fdbc4e89005fff7d56d2a5226c
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-10-04 00:00:52 -0500

    style(catalog): apply 2 lines indentation

--- src/main/java/org/example/seed/catalog/ChefStatus.java
+++ src/main/java/org/example/seed/catalog/ChefStatus.java
@@ -3,6 +3,6 @@ package org.example.seed.catalog;
 /**
  * Created by PINA on 14/06/2017.
  */
-public enum  ChefStatus {
-    REGISTERED, ACTIVATED, CANCELLED
+public enum ChefStatus {
+  REGISTERED, ACTIVATED, CANCELLED
 }
--- src/main/java/org/example/seed/catalog/DishType.java
+++ src/main/java/org/example/seed/catalog/DishType.java
@@ -4,5 +4,5 @@ package org.example.seed.catalog;
  * Created by PINA on 01/07/2017.
  */
 public enum DishType {
-    FOODS, DRINKS, DESSERTS
+  FOODS, DRINKS, DESSERTS
 }
