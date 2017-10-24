1bf64d3bcfaf503aa4613d9497c6de778bedd88a
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-10-23 23:55:34 -0500

    feat(rest): remove chef rest as interface and set into concrete class

--- src/main/java/org/example/seed/rest/ChefRest.java
+++ src/main/java/org/example/seed/rest/ChefRest.java
@@ -1,38 +1,63 @@
 package org.example.seed.rest;
 
-import org.example.seed.event.chef.CatalogChefEvent;
-import org.example.seed.event.chef.CreateChefEvent;
-import org.example.seed.event.chef.ResponseChefEvent;
-import org.example.seed.event.chef.UpdateChefEvent;
+import org.example.seed.event.chef.*;
+import org.example.seed.group.chef.ChefCreateGroup;
+import org.example.seed.group.chef.ChefUpdateGroup;
+import org.example.seed.service.ChefService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.ExecutionException;
 
 /**
- * Created by PINA on 15/06/2017.
+ * Created by PINA on 25/06/2017.
  */
+@RestController
 @RequestMapping(path = "/chefs")
-public interface ChefRest {
+public class ChefRest {
+
+  private final ChefService chefService;
+
+  @Autowired
+  public ChefRest(final ChefService chefService) {
+    this.chefService = chefService;
+  }
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  Mono<CatalogChefEvent> getChefs(final int page, final int limit) throws ExecutionException, InterruptedException;
+  public Mono<CatalogChefEvent> getChefs(@RequestParam("page") final int page, @RequestParam("limit") final int limit)
+    throws ExecutionException, InterruptedException {
+    return Mono.justOrEmpty(this.chefService.requestChefs(RequestAllChefEvent.builder().page(page).limit(limit).build()).get());
+  }
 
   @PostMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  Mono<ResponseChefEvent> createChef(final CreateChefEvent event) throws ExecutionException, InterruptedException;
+  public Mono<ResponseChefEvent> createChef(@RequestBody @Validated(value = {ChefCreateGroup.class}) final CreateChefEvent event)
+    throws ExecutionException, InterruptedException {
+    return Mono.justOrEmpty(this.chefService.createChef(event).get());
+  }
 
   @GetMapping(value = "/{id}")
   @ResponseStatus(HttpStatus.OK)
-  Mono<ResponseChefEvent> getChef(final String id) throws ExecutionException, InterruptedException;
+  public Mono<ResponseChefEvent> getChef(@PathVariable("id") final String id)
+    throws ExecutionException, InterruptedException {
+    return Mono.justOrEmpty(this.chefService.requestChef(RequestChefEvent.builder().id(id).build()).get());
+  }
 
   @PutMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  Mono<ResponseChefEvent> updateChef(final UpdateChefEvent event) throws ExecutionException, InterruptedException;
+  public Mono<ResponseChefEvent> updateChef(@RequestBody @Validated(value = {ChefUpdateGroup.class}) final UpdateChefEvent event)
+    throws ExecutionException, InterruptedException {
+    return Mono.justOrEmpty(this.chefService.updateChef(event).get());
+  }
 
   @DeleteMapping(value = "/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  Mono<ResponseChefEvent> deleteChef(final String id) throws ExecutionException, InterruptedException;
+  public Mono<ResponseChefEvent> deleteChef(@PathVariable("id") final String id)
+    throws ExecutionException, InterruptedException {
+    return Mono.justOrEmpty(this.chefService.deleteChef(DeleteChefEvent.builder().id(id).build()).get());
+  }
 }
