b7d4ba36198e084393fe216ab638d70979537cb3
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-10-25 18:46:56 -0500

    fix(repository): add existByEmail method

--- src/main/java/org/example/seed/repository/ChefRepository.java
+++ src/main/java/org/example/seed/repository/ChefRepository.java
@@ -1,5 +1,6 @@
 package org.example.seed.repository;
 
+import org.example.seed.catalog.ChefStatus;
 import org.example.seed.entity.ChefEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,11 @@ import java.util.Optional;
 public interface ChefRepository extends JpaRepository<ChefEntity, String> {
 
   Optional<ChefEntity> findById(final String id);
+
+  Optional<ChefEntity> findByIdAndStatus(final String id, final ChefStatus status);
+
+  @Query("SELECT CASE WHEN COUNT(ce) > 0 THEN true ELSE false END FROM ChefEntity ce WHERE ce.account.email = ?1")
+  boolean existsByEmail(final String email);
 
   @Query("SELECT ce FROM ChefEntity ce LEFT JOIN ce.account AS ae JOIN ce.dishes AS se WHERE ce.active = TRUE AND se.id = ?1")
   ChefEntity findOneByDish(final String idDish);
