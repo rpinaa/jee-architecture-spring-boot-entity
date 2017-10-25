a567dfc714c84c4baeb926b6bd9b449704802c55
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-10-25 18:46:02 -0500

    feat(event): create new register chef event

--- src/main/java/org/example/seed/event/chef/RegisterEvent.java
+++ src/main/java/org/example/seed/event/chef/RegisterEvent.java
@@ -3,15 +3,11 @@ package org.example.seed.event.chef;
 import lombok.*;
 import org.example.seed.domain.Chef;
 import org.example.seed.event.CreateEvent;
-import org.example.seed.group.chef.ChefCreateGroup;
+import org.example.seed.group.chef.ChefConfirmGroup;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
-
-/**
- * Created by PINA on 22/05/2017.
- */
 
 @Data
 @Builder
@@ -19,9 +15,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 @AllArgsConstructor
 @XmlRootElement
 @EqualsAndHashCode(callSuper = true)
-public class CreateChefEvent extends CreateEvent {
+public class RegisterEvent extends CreateEvent {
 
   @Valid
-  @NotNull(groups = {ChefCreateGroup.class})
+  @NotNull(groups = {ChefConfirmGroup.class})
   private Chef chef;
 }
