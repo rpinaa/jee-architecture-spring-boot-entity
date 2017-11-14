0861d1f72dd3e85374c20accbba6642c8a956b2f
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-11-13 18:54:30 -0600

    feat(test): create chef and client rest IT

---
+++ src/test/java/org/example/seed/rest/ChefRestITest.java
@@ -0,0 +1,29 @@
+package org.example.seed.rest;
+
+import org.example.seed.Application;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, properties = {"spring.cloud.config.profile:local"})
+public class ChefRestITest {
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setUp() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+}
