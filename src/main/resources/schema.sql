96d6a01ad5bd6418cf5d9f039d58db0d2bf08fbf
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-11-22 00:09:05 -0600

    chore(schema): finishe sql schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,4 @@
 --
--- Base de datos: `test`
---
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `t_account`
 --
 
@@ -65,6 +59,27 @@ CREATE TABLE IF NOT EXISTS `t_chef` (
   CONSTRAINT `fk_chef_account`
   FOREIGN KEY (`fk_id_account`)
   REFERENCES `t_account` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_telephone`
+--
+
+CREATE TABLE IF NOT EXISTS `t_telephone` (
+  `id` varchar(36) NOT NULL,
+  `lada` varchar(5) DEFAULT NULL,
+  `name` varchar(15) DEFAULT NULL,
+  `number` varchar(12) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `fk_id_chef` varchar(36) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_telephone_chef`
+  FOREIGN KEY (`fk_id_chef`)
+  REFERENCES `t_chef` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
@@ -148,7 +163,22 @@ CREATE TABLE IF NOT EXISTS `t_order` (
   `fk_id_address` varchar(36) DEFAULT NULL,
   `fk_id_chef` varchar(36) DEFAULT NULL,
   `fk_id_client` varchar(36) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_order_address`
+  FOREIGN KEY (`fk_id_address`)
+  REFERENCES `t_address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_chef`
+  FOREIGN KEY (`fk_id_chef`)
+  REFERENCES `t_chef` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_client`
+  FOREIGN KEY (`fk_id_client`)
+  REFERENCES `t_client` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 -- --------------------------------------------------------
@@ -166,89 +196,15 @@ CREATE TABLE IF NOT EXISTS `t_package` (
   `quantity` int(11) DEFAULT NULL,
   `fk_id_dish` varchar(36) DEFAULT NULL,
   `fk_id_order` varchar(36) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_package_dish`
+  FOREIGN KEY (`fk_id_dish`)
+  REFERENCES `t_dish` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_package_order`
+  FOREIGN KEY (`fk_id_order`)
+  REFERENCES `t_order` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `t_telephone`
---
-
-CREATE TABLE IF NOT EXISTS `t_telephone` (
-  `id` varchar(36) NOT NULL,
-  `lada` varchar(5) DEFAULT NULL,
-  `name` varchar(15) DEFAULT NULL,
-  `number` varchar(12) DEFAULT NULL,
-  `type` varchar(10) DEFAULT NULL,
-  `fk_id_chef` varchar(36) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
-
-
---
--- Indices de la tabla `t_order`
---
-ALTER TABLE `t_order`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKmdxjxqbgrjuka1o0ryhd8lh6s` (`fk_id_address`),
-  ADD KEY `FKnnkk737w7hck6mex2lhc5gokn` (`fk_id_chef`),
-  ADD KEY `FK9i6meo1rw967j70bvksbemlh1` (`fk_id_client`);
-
---
--- Indices de la tabla `t_package`
---
-ALTER TABLE `t_package`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK80mv05lhoewvvl63ctw0ll2mw` (`fk_id_dish`),
-  ADD KEY `FKl9iocvn3nnpaijyegwraonh4k` (`fk_id_order`);
-
---
--- Indices de la tabla `t_telephone`
---
-ALTER TABLE `t_telephone`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK83tvpf5obvqh6pi6k8su3h3tm` (`fk_id_chef`);
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `t_chef`
---
-ALTER TABLE `t_chef`
-  ADD CONSTRAINT `FKi8vy65n7ve0x77hlqbw402xpr` FOREIGN KEY (`fk_id_account`) REFERENCES `t_account` (`id`);
-
---
--- Filtros para la tabla `t_client`
---
-ALTER TABLE `t_client`
-  ADD CONSTRAINT `FK9ltyy5tpd8prmvk0k2rsb2oro` FOREIGN KEY (`fk_id_telephone`) REFERENCES `t_telephone` (`id`);
-
---
--- Filtros para la tabla `t_dish`
---
-ALTER TABLE `t_dish`
-  ADD CONSTRAINT `FKdiw6p7gelub70axlkvo8ismn8` FOREIGN KEY (`fk_id_chef`) REFERENCES `t_chef` (`id`);
-
---
--- Filtros para la tabla `t_order`
---
-ALTER TABLE `t_order`
-  ADD CONSTRAINT `FK9i6meo1rw967j70bvksbemlh1` FOREIGN KEY (`fk_id_client`) REFERENCES `t_client` (`id`),
-  ADD CONSTRAINT `FKmdxjxqbgrjuka1o0ryhd8lh6s` FOREIGN KEY (`fk_id_address`) REFERENCES `t_address` (`id`),
-  ADD CONSTRAINT `FKnnkk737w7hck6mex2lhc5gokn` FOREIGN KEY (`fk_id_chef`) REFERENCES `t_chef` (`id`);
-
---
--- Filtros para la tabla `t_package`
---
-ALTER TABLE `t_package`
-  ADD CONSTRAINT `FK80mv05lhoewvvl63ctw0ll2mw` FOREIGN KEY (`fk_id_dish`) REFERENCES `t_dish` (`id`),
-  ADD CONSTRAINT `FKl9iocvn3nnpaijyegwraonh4k` FOREIGN KEY (`fk_id_order`) REFERENCES `t_order` (`id`);
-
---
--- Filtros para la tabla `t_telephone`
---
-ALTER TABLE `t_telephone`
-  ADD CONSTRAINT `FK83tvpf5obvqh6pi6k8su3h3tm` FOREIGN KEY (`fk_id_chef`) REFERENCES `t_chef` (`id`);
